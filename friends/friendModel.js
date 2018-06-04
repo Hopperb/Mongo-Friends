a25ea0789d30125c3220036f8c97ec3a4a025b1e
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-04 16:41:28 -0400

    - created Friend Schema

---
+++ friends/friendModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const FriendSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: number,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+const friendModel = mongoose.model('Friend', FriendSchema);
+module.exports = friendModel;
